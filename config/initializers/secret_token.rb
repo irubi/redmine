433054e8ec626526a6741974dcaa7c59ce30639a
apple <elite.ljp@gmail.com>
2013-08-14 10:00:25 +0800

    fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedmineApp::Application.config.secret_token = '2e591c8d596c28055cd4c33556c945e599434281c685cff525f32e3044155a39fbc400327f2dadb840a6f688f9b9878feb7b90420116b009160fa730548844c2'
